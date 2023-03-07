61d6e09689cffa7c2ceff60b3307c709a251394a
YatinBajajOP <yatin19csu360@ncuindia.edu>
2023-03-08 01:47:11 +0530

    fifth trial commit

--- addPosts.py
+++ addPosts.py
@@ -4,27 +4,27 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import time
 import random
-from dotenv import load_dotenv
-import os
-# import streamlit as st
-load_dotenv()
+# from dotenv import load_dotenv
+# import os
+# # import streamlit as st
+# load_dotenv()
 
-CONFIG = {
-  "type": os.getenv("type"),
-  "project_id": os.getenv("project_id"),
-  "private_key_id": os.getenv("private_key_id"),
-  "private_key": os.getenv("private_key"),
-  "client_email": os.getenv("client_email"),
-  "client_id": os.getenv("client_id"),
-  "auth_uri": os.getenv("auth_uri"),
-  "token_uri": os.getenv("token_uri"),
-  "auth_provider_x509_cert_url": os.getenv("auth_provider_x509_cert_url"),
-  "client_x509_cert_url": os.getenv("client_x509_cert_url")
-}
+# CONFIG = {
+#   "type": os.getenv("type"),
+#   "project_id": os.getenv("project_id"),
+#   "private_key_id": os.getenv("private_key_id"),
+#   "private_key": os.getenv("private_key"),
+#   "client_email": os.getenv("client_email"),
+#   "client_id": os.getenv("client_id"),
+#   "auth_uri": os.getenv("auth_uri"),
+#   "token_uri": os.getenv("token_uri"),
+#   "auth_provider_x509_cert_url": os.getenv("auth_provider_x509_cert_url"),
+#   "client_x509_cert_url": os.getenv("client_x509_cert_url")
+# }
 
-print(CONFIG)
+# print(CONFIG)
 # Initialize Firebase app
-cred = credentials.Certificate(CONFIG)
+cred = credentials.Certificate("/etc/secrets/config.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://goose-village.firebaseio.com'
 })
