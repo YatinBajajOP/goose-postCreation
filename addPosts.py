1f9b4ac4b763183a3c0a13b31bfaff9002da785a
YatinBajajOP <yatin19csu360@ncuindia.edu>
2023-03-12 19:34:59 +0530

    Added UI

--- addPosts.py
+++ addPosts.py
@@ -4,6 +4,8 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import time
 import random
+import streamlit as st
+
 # from dotenv import load_dotenv
 # import os
 # # import streamlit as st
@@ -24,24 +26,40 @@ import random
 
 # print(CONFIG)
 # Initialize Firebase app
-cred = credentials.Certificate("/etc/secrets/config.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://goose-village.firebaseio.com'
-})
+if not firebase_admin._apps:
+
+    cred = credentials.Certificate("/etc/secrets/config.json")
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://goose-village.firebaseio.com'
+    })
 # db = firebase_admin.db
 db = firestore.client()
+
+st.title("Add a csv file containing postData")
+uploaded_file = st.file_uploader("Upload the postData file")
+if uploaded_file is not None:
+    #read csv
+    df=pd.read_csv(uploaded_file)
+
+else:
+    st.warning("You need to upload a csv or excel file.")
+
 # Read data from Excel file
-df = pd.read_csv("postData.csv")
+# df = pd.read_csv("postData.csv")
 # print(df)
 
-# Convert data to dictionary
-data = df.to_dict(orient="records")
-# print(data)
+delay = st.number_input("Enter the delay(in seconds) in posts", step=1, value=5)
 
-# # Upload data to Firebase Realtime Database with a 5-second delay between each record
-ref = db.collection("posts")
-for i, record in enumerate(data):
-    delay = random.randint(2, 5)
-    ref.add(record)
-    print(f"Post {i} added.")
-    time.sleep(delay)
+if st.button("Add posts to firebase"):
+    # Convert data to dictionary
+    data = df.to_dict(orient="records")
+    # print(data)
+
+    # # Upload data to Firebase Realtime Database with a 5-second delay between each record
+    ref = db.collection("posts")
+    for i, record in enumerate(data):
+        delay = random.randint(delay, delay+2)
+        ref.add(record)
+        st.write(f"Post {i} added.")
+        time.sleep(delay)
+    st.success("All posts added successfully")
